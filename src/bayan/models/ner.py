24ed09b252d9f43a35b9b331910c238ac1dc1237
Salma Alfehaid <salmaalfehaid67@gmail.com>
2026-09-10 17:59:35 +0300

    Implement label alignment for NER
    Implemented alignment of BIO labels to subwords and masked non-first pieces with -100.

--- src/bayan/models/ner.py
+++ src/bayan/models/ner.py
@@ -1,6 +1,20 @@
-"""Lab 3 starter: NER label alignment."""
+"""Lab 3B: NER label alignment."""
 
 
 def align_labels(word_ids, word_labels):
-    # TODO(Lab 3B): align BIO labels to subwords and mask non-first pieces with -100.
-    raise NotImplementedError
+    aligned = []
+    previous_word_id = None
+
+    for word_id in word_ids:
+        if word_id is None:
+            aligned.append(-100)
+
+        elif word_id != previous_word_id:
+            aligned.append(word_labels[word_id])
+
+        else:
+            aligned.append(-100)
+
+        previous_word_id = word_id
+
+    return aligned
